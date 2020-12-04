1b55314aee7387cdb37040a2e894c847b0882729
IzzatSotimkulov <izzatsotimkulov@gmail.com>
2020-12-04 18:23:24 +0500

    Made form

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
+@app.route('/sign-up/', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('sign-up.html')
+
+
+@app.route('/sign-in/', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('sign-in.html')
 
 
 
