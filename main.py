aa498462a241c22ca1adc3e906cf6aa0c91c5b0f
IzzatSotimkulov <izzatsotimkulov@gmail.com>
2020-12-05 13:26:39 +0500

    Test

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketplace.db'
+app.config['TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, default='')
+    okonh_code = db.Column(db.Integer, nullable=False)
+    date = datetime.now()
+
 
 @app.route('/')
 def index():
@@ -30,6 +42,17 @@ def home():
         pass
     else:
         return render_template('user.html')
+
+
+@app.route('/companies/', methods=['POST', 'GET'])
+def companies():
+    if request.method == "POST":
+        pass
+    else:
+        companies = Company.query.order_by().all()
+        return render_template('companies.html', data = companies)
+
+
 
 
 app.run(debug=True)
